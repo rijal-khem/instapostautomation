3b023e17a1ea7088924a5093a7a5e93186c7ef79
rizal <rijalkhem187@gmail.com>
2023-03-06 20:13:15 -0500

    first commit

---
+++ src/main/java/com/rizzkitch/instapostautomation/InstapostautomationApplication.java
@@ -0,0 +1,13 @@
+package com.rizzkitch.instapostautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstapostautomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstapostautomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rizzkitch/instapostautomation/InstapostautomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rizzkitch.instapostautomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstapostautomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
